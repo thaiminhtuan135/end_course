199cf8ed3de51493f4bed39fdf58696bbcd31025
thaiminhtuan135 <tuana11ht@gmail.com>
2023-04-27 23:57:34 +0700

    add image course

--- src/main/java/example/end_course/controller/CourseController.java
+++ src/main/java/example/end_course/controller/CourseController.java
@@ -10,13 +10,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Objects;
 
 @RestController
 @CrossOrigin
-@RequestMapping("admin/course")
+@RequestMapping("/admin/course")
 public class CourseController {
     @Autowired
     private CourseService courseService;
@@ -24,22 +32,61 @@ public class CourseController {
     private TypeCourseService typeCourseService;
     private final com.google.gson.Gson gson = Gson.gson();
 
+    private static final Path CURRENT_FOLDER = Paths.get(System.getProperty("user.dir"));
+
     @GetMapping("/list")
     public List<Course> getCourses() {
         return courseService.getCourses();
     }
 
     @PostMapping("/create/type-course/{typeCourseId}")
-    public ResponseEntity<Course> create(@RequestBody String course, @PathVariable Integer typeCourseId) {
+    public ResponseEntity<Course> create(
+            @RequestParam String name,
+            @RequestParam LocalDate time,
+            @RequestParam String introduce,
+            @RequestParam String content,
+            @RequestParam Integer price,
+            @RequestParam Integer amount_student,
+            @RequestParam Integer amount_subject,
+            @RequestParam(value = "image",required = false) MultipartFile image,
+            @PathVariable Integer typeCourseId) throws IOException {
+        TypeCourse typeCourse = typeCourseService.getTypeCourseById(typeCourseId).get();
+        Course course1 = new Course();
+        System.out.println(CURRENT_FOLDER); // C:\Users\ADMIN\Desktop\java\end_course
+        if (image != null) {
+            System.out.println(image.getOriginalFilename());
+            Path staticPath = Paths.get("static"); // static
+            Path imagePath = Paths.get("images");   // images
 
-        return typeCourseService.getTypeCourseById(typeCourseId).map(typeCourse -> {
-            Course course1 = gson.fromJson(course, Course.class);
-            course1.setTypeCourse(typeCourse);
-            course1.setTypeCourse_id(typeCourse.getId());
-            return new ResponseEntity<>(courseService.save(course1), HttpStatus.CREATED);
-        }).orElseGet(
-                () -> new ResponseEntity<>(HttpStatus.NOT_FOUND)
-        );
+            if (!Files.exists(CURRENT_FOLDER.resolve(staticPath).resolve(imagePath))) {
+                Files.createDirectories(CURRENT_FOLDER.resolve(staticPath).resolve(imagePath));
+            }
+
+            int hashCode = Objects.requireNonNull(image.getOriginalFilename()).hashCode();
+
+            Path file = CURRENT_FOLDER.resolve(staticPath)
+                    .resolve(imagePath).resolve(hashCode + "." + image.getOriginalFilename());
+            System.out.println(file); // C:\Users\ADMIN\Desktop\java\end_course\static\images\...
+
+            try (OutputStream os = Files.newOutputStream(file)) {
+                os.write(image.getBytes());
+            }
+            course1.setImage(imagePath.resolve(hashCode+"."+image.getOriginalFilename()).toString());
+        }
+
+        course1.setName(name);
+        course1.setTime(time);
+        course1.setIntroduce(introduce);
+        course1.setContent(content);
+        course1.setPrice(price);
+        course1.setAmount_student(amount_student);
+        course1.setAmount_subject(amount_subject);
+
+        course1.setTypeCourse(typeCourse);
+        course1.setTypeCourse_id(typeCourseId);
+
+        return new ResponseEntity<>(courseService.save(course1), HttpStatus.CREATED);
+
     }
 
     @PutMapping("/{courseId}/edit/type-course/{typeCourseId}")
