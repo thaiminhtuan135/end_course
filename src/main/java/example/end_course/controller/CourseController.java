d45ee754f350940aea1c99dd5370953334270961
thaiminhtuan135 <tuana11ht@gmail.com>
2023-04-29 23:47:25 +0700

    fix update course with @RequestParam

--- src/main/java/example/end_course/controller/CourseController.java
+++ src/main/java/example/end_course/controller/CourseController.java
@@ -24,7 +24,7 @@ import java.util.Objects;
 
 @RestController
 @CrossOrigin
-@RequestMapping("/admin/course")
+@RequestMapping("/api/v1/admin/course")
 public class CourseController {
     @Autowired
     private CourseService courseService;
@@ -90,22 +90,80 @@ public class CourseController {
     }
 
     @PutMapping("/{courseId}/edit/type-course/{typeCourseId}")
-    public ResponseEntity<Course> update(@RequestBody String course,
-                                         @PathVariable Integer courseId,
-                                         @PathVariable Integer typeCourseId) {
+    public ResponseEntity<Course> update(@RequestParam String name, @RequestParam LocalDate time,
+                                         @RequestParam String introduce, @RequestParam String content,
+                                         @RequestParam Integer price, @RequestParam Integer amount_student,
+                                         @RequestParam Integer amount_subject,
+                                         @RequestParam(value = "image",required = false) MultipartFile image,
+                                         @PathVariable Integer typeCourseId,
+                                         @PathVariable Integer courseId)  {
 
         return courseService.getCourseById(courseId).map(course1 -> {
 
             try {
                 TypeCourse typeCourse = typeCourseService.getTypeCourseById(typeCourseId).get();
-                Course course2 = gson.fromJson(course, Course.class);
-                course2.setId(course1.getId());
-                course2.setTypeCourse(typeCourse);
-                course2.setTypeCourse_id(typeCourse.getId());
-                return new ResponseEntity<>(courseService.save(course2), HttpStatus.OK);
+
+//                Path staticPath = Paths.get("static");
+//                Path imagePath = Paths.get("images");
+//                if (course1.getImage() == null) {
+//                    if (image != null) {
+//                        if (!Files.exists(CURRENT_FOLDER.resolve(staticPath).resolve(imagePath))) {
+//                            Files.createDirectories(CURRENT_FOLDER.resolve(staticPath).resolve(imagePath));
+//                        }
+//
+//                        int hashCode = Objects.requireNonNull(image.getOriginalFilename()).hashCode();
+//
+//                        Path file = CURRENT_FOLDER.resolve(staticPath)
+//                                .resolve(imagePath).resolve(hashCode + "." + image.getOriginalFilename());
+//
+//                        try (OutputStream os = Files.newOutputStream(file)) {
+//                            os.write(image.getBytes());
+//                        }
+//                        course1.setImage(imagePath.resolve(hashCode+"."+image.getOriginalFilename()).toString());
+//                    }
+//                }
+//                else {
+//                    if (image != null) {
+//                        if (!Files.exists(CURRENT_FOLDER.resolve(staticPath).resolve(imagePath))) {
+//                            Files.createDirectories(CURRENT_FOLDER.resolve(staticPath).resolve(imagePath));
+//                        }
+//
+//                        int hashCode = Objects.requireNonNull(image.getOriginalFilename()).hashCode();
+//
+//                        Path file = CURRENT_FOLDER.resolve(staticPath)
+//                                .resolve(imagePath).resolve(hashCode + "." + image.getOriginalFilename());
+//                        System.out.println(file); // C:\Users\ADMIN\Desktop\java\end_course\static\images\...
+//
+//                        try (OutputStream os = Files.newOutputStream(file)) {
+//                            os.write(image.getBytes());
+//                        }
+//                        course1.setImage(imagePath.resolve(hashCode+"."+image.getOriginalFilename()).toString());
+//                    }
+//                    else {
+//                        Path fileToDelete = Paths.get("src/main/resources/static/" + course1.getImage());
+//                        if (Files.exists(fileToDelete)) {
+//                            Files.delete(fileToDelete);
+//                        }
+//                    }
+//                }
+                course1.setName(name);
+                course1.setTime(time);
+                course1.setIntroduce(introduce);
+                course1.setContent(content);
+                course1.setPrice(price);
+                course1.setAmount_student(amount_student);
+                course1.setAmount_subject(amount_subject);
+
+                course1.setTypeCourse(typeCourse);
+                course1.setTypeCourse_id(typeCourseId);
+
+                return new ResponseEntity<>(courseService.save(course1), HttpStatus.OK);
             } catch (NoSuchElementException e) {
                 return new ResponseEntity<Course>(HttpStatus.NOT_FOUND);
             }
+//            catch (IOException e) {
+//                throw new RuntimeException(e);
+//            }
         }).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
